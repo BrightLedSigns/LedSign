2db69b2e7bdf94a8a67b31b5c4308e79c6768409
brightledsigns <sales@brightledsigns.com>
2014-01-02 08:42:22 -0600

    doc changes

--- lib/LedSign/Mini.pm
+++ lib/LedSign/Mini.pm
@@ -1425,18 +1425,17 @@ B<packetdelay>: An amount of time, in seconds, to wait, between sending packets 
 
 =back
 
-
-  
-  # typical use on a windows machine
   $buffer->sendQueue(
       device => "COM4"
-  );
-  # typical use on a unix/linux machine
+  ); # typical use on a windows machine
+  
   $buffer->sendQueue(
       device => "/dev/ttyUSB0"
-  );
+  ); # typical use on a unix/linux machine
+
   # using optional arguments, set baudrate to 9600, and sleep 1/2 a second
   # between sending packets.  
+  
   $buffer->sendQueue(
       device => "COM8",
       baudrate => "9600",
@@ -1446,10 +1445,12 @@ B<packetdelay>: An amount of time, in seconds, to wait, between sending packets 
 Note that if you have multiple connected signs, you can send to them without creating a new object:
 
   
-  # send to the first sign
   $buffer->sendQueue(device => "COM4");
-  # send to another sign
+  # send to the first sign
+  
   $buffer->sendQueue(device => "COM6");
+  # send to another sign
+  
   # send to a badge connected on COM7
   #   this works fine for plain text, but won't work well for
   #   pictures and icons...you'll have to create a new
